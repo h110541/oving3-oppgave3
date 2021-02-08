b041ad8c07b448c92a52f95208a04e56f3ceb6af
h110541 <110541@stud.hvl.no>
2021-02-08 13:44:31 +0100

    Filmarkiv2 ferdig

--- src/no/hvl/dat102/Filmarkiv2.java
+++ src/no/hvl/dat102/Filmarkiv2.java
@@ -4,46 +4,124 @@ import no.hvl.dat102.adt.FilmarkivADT;
 
 public class Filmarkiv2 implements FilmarkivADT {
 
+	private int antall;
+	private LinearNode<Film> start;
+
+	public Filmarkiv2() {
+		antall = 0;
+		start = null;
+	}
+
 	@Override
 	public Film[] hentFilmTabell() {
-		// TODO Auto-generated method stub
-		return null;
+		Film[] filmer = new Film[antall];
+		LinearNode<Film> node = start;
+
+		for(int i = 0; i < filmer.length; i++) {
+			filmer[i] = node.getElement();
+			node = node.getNeste();
+		}
+
+		return filmer;
 	}
 
 	@Override
 	public void leggTilFilm(Film nyFilm) {
-		// TODO Auto-generated method stub
-
+		LinearNode<Film> nyNode = new LinearNode<Film>(nyFilm);
+		nyNode.setNeste(start);
+		start = nyNode;
+		antall++;
 	}
 
 	@Override
 	public boolean slettFilm(int filmnr) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean funnet = false;
+		LinearNode<Film> current = start;
+		LinearNode<Film> prev = null;
+
+		while(current != null && !funnet) {
+			if(current.getElement().getFilmnr() == filmnr) {
+				funnet = true;
+			} else {
+				prev = current;
+				current = current.getNeste();
+			}
+		}
+
+		if(funnet) {
+			if(prev == null) {
+				start = start.getNeste();
+			} else {
+				prev.setNeste(current.getNeste());
+			}
+
+			antall--;
+		}
+
+		return funnet;
 	}
 
 	@Override
 	public Film[] soekTittel(String delstreng) {
-		// TODO Auto-generated method stub
-		return null;
+		String delstrengLower = delstreng.toLowerCase();
+		Film[] resultatTab = new Film[antall];
+		int antallMatches = 0;
+
+		LinearNode<Film> node = start;
+		while(node != null) {
+			if(node.getElement().getTittel().toLowerCase().contains(delstrengLower)) {
+				resultatTab[antallMatches] = node.getElement();
+				antallMatches++;
+			}
+		}
+
+		return trimTab(resultatTab, antallMatches);
 	}
 
 	@Override
 	public Film[] soekProdusent(String delstreng) {
-		// TODO Auto-generated method stub
-		return null;
+		String delstrengLower = delstreng.toLowerCase();
+		Film[] resultatTab = new Film[antall];
+		int antallMatches = 0;
+
+		LinearNode<Film> node = start;
+		while(node != null) {
+			if(node.getElement().getProdusent().toLowerCase().contains(delstrengLower)) {
+				resultatTab[antallMatches] = node.getElement();
+				antallMatches++;
+			}
+		}
+
+		return trimTab(resultatTab, antallMatches);
 	}
 
 	@Override
 	public int antall(Sjanger sjanger) {
-		// TODO Auto-generated method stub
-		return 0;
+		int antallSjanger = 0;
+		LinearNode<Film> node = start;
+
+		while(node != null) {
+			if(node.getElement().getSjanger() == sjanger) {
+				antallSjanger++;
+			}
+		}
+
+		return antallSjanger;
 	}
 
 	@Override
 	public int antall() {
-		// TODO Auto-generated method stub
-		return 0;
+		return antall;
+	}
+
+	private Film[] trimTab(Film[] tab, int n) {
+		Film[] filmtab2 = new Film[n];
+
+		for(int i = 0; i < n; i++) {
+			filmtab2[i] = tab[i];
+		}
+
+		return filmtab2;
 	}
 
 }
